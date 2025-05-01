953fab32661ebf549d0b45d51d8cc13de09ce97f
홍왕기 <144398371+hongwangki@users.noreply.github.com>
2025-05-01 11:00:03 +0900

    init project commit

---
+++ LMS/src/test/java/Project3/LMS/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package Project3.LMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
